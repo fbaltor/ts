9ca2ff434542f224da851bf24da8d69b7af4831b
Felipe Baltor <fbaltor@gmail.com>
2021-07-11 20:46:03 -0300

    GraphQL branch created.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-const defaultWorld = 'world';
+import * as express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
-export function hello(world: string = defaultWorld): string {
-  return `Hello, ${world}! `;
-}
+const PORT = 4000;
+
+const schema = buildSchema(`
+                          type Query {
+                            hello: String
+                          }
+                          `);
+
+const root = {
+  hello: () => 'Hello World!',
+};
+
+const app = express.default();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () =>
+  console.log(`GraphQL server listening on port ${PORT}!`)
+);
